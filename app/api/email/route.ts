f258cb71e6e94166cd5a7dbd60fa843839638418
Chinwike <maduabuchichizzy@gmail.com>
2023-05-03 15:43:05 +0100

    feat(api): verify that name and email fields aren't before sending email

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,38 +1,50 @@
 import nodemailer from 'nodemailer'
 import { render } from '@react-email/render'
-import { NextApiResponse } from 'next';
 import { SpringSalesMail } from '../../../react-email-starter/emails/spring-sales'
 
-export async function POST(req: Request, res: NextApiResponse){
+export async function POST(req: Request) {
   const { name, email } = await req.json()
 
-  const emailHtml = render(SpringSalesMail({ userName: name }));
-  
+  const emailHtml = render(SpringSalesMail({ userName: name }))
+
   const transporter = nodemailer.createTransport({
-   service: "gmail",
+    service: 'gmail',
     auth: {
       user: process.env.GMAIL_USER,
       pass: process.env.GMAIL_PASSWORD,
     },
     tls: {
-          rejectUnauthorized: false
-      }
-  });
+      rejectUnauthorized: false,
+    },
+  })
 
   const mailOptions = {
     from: process.env.GMAIL_USER,
     to: email,
     subject: "Spring flower sales💐 Don't miss out!",
     html: emailHtml,
-  };
+  }
+
+  if (!name || !email) {
+    return new Response(
+      JSON.stringify({ message: 'Please submit your name and email' }),
+      { status: 400 }
+    )
+  }
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.error(error);
-      res.status(500).send("Error: Could not send email");
-    } else {
-      console.log("Email sent: " + info.response);
-      res.status(200).send("Email sent successfully");
+      console.error(error)
+      return new Response(
+        JSON.stringify({ message: 'Error: Could not send email' }),
+        { status: 400 }
+      )
     }
-  });
+
+    console.log('Email sent: ' + info.response)
+    return new Response(
+      JSON.stringify({ message: 'Email sent successfully' }),
+      { status: 200 }
+    )
+  })
 }
